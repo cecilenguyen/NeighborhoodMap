52e64526db3e09872d0b4703c900e28dcc7d48db
Nguyen, Ngoc-uyen (nn755d) <nn755d@us.att.com>
2017-12-01 14:15:54 -0800

    filtering for both names on left panel and markers on map

--- maps.js
+++ maps.js
@@ -103,12 +103,15 @@ function filter() {
     ul = document.getElementById("locationList");
     li = ul.getElementsByTagName("li");
     for (i = 0; i < li.length; i++) {
+    	// put markers back if match search
+    	markers[i].setVisible(true);
        // a = li[i].getElementsByTagName("a")[0];
         if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
-
+            // hide markers that don't match search
+            markers[i].setVisible(false);
         }
     }
 }
