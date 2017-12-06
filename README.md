f60b36b049c28ae4b38de6a7e7c96281f8b22921
Uyen Nguyen <Uyen@Uyens-MBP.hitronhub.home>
2017-12-05 20:54:22 -0800

    readme

---
+++ README.md
@@ -0,0 +1,37 @@
+# Udacity Project 5 - Neighborhood Map
+
+A simple map application depicting 5 locations centralized around Seattle, WA. Locations can be searched for from the left panel side bar and the markers on the map will be filtered simultaneously. Clicking on a location marker will show a Google street view as well as the address. This map application is populated using the Google Maps API and the FourSquare API.
+
+## Download
+
+The files for this program may be downloaded or cloned from https://github.com/cecilenguyen/NeighborhoodMap.
+
+Use the following command in the command terminal:
+
+`$ git clone https://github.com/cecilenguyen/NeighborhoodMap.git`
+
+## System Requirements
+
+Browser such as IE, Firefox, or Safari
+
+You may need [Python 3.6.3](https://www.python.org/downloads/) to run the application on a sample server if you encounter CORS issues.
+
+## Quick Start
+
+After downloading or cloning the program files, open the directory the files are downloaded to. Double click the index.html file and it should open the webpage in your default browser.
+
+You can also load the webpage by copying and pasting the filepath of portfolio.html into your browser's address bar.
+
+If you encounter cross origin issues, you can run the application on a local server. Open a command prompt and navigate to the location of the Neighborhood Map code. Run the command `$ python -m SimpleHTTPServer 8000`. In your browser, type "localhost:8000" into the navigation bar. The application should now run on that port.
+
+## References
+
+Google Maps API starter code from Udacity
+https://developers.google.com/maps/documentation/javascript/
+https://developer.foursquare.com/
+http://knockoutjs.com/documentation/introduction.html
+https://www.w3schools.com/jsref/event_onerror.asp
+https://stackoverflow.com/questions/7339200/bounce-a-pin-in-google-maps-once
+
+
+
--- maps.js
+++ maps.js
@@ -1,161 +1,160 @@
-// global variables
+// global variable
 var map;
 
-function AppViewModel() {
-
-	var self = this;
-
-	// put marker info in infowindow
-	this.populateInfoWindow = function(marker, infowindow){
-			
-			if(infowindow.marker != marker){
-				infowindow.marker = marker;
-				infowindow.setContent('<div>' + marker.title + '</div>');
-				infowindow.open(map, marker);
-				// make sure marker property cleared if infowindow is closed
-				infowindow.addListener('closeclick', function(){
-					infowindow = null;
-				});
-			}
-
-			
-			// foursquare api
-            clientID = "PVY15A1THVOBNAJETF2AZDRFGIY5YJKARWIAU2YWOIS2UQWH";
-            clientSecret = "XPETPWZCF0P45QLZOYUBOY5W55SPP1JYSJSEL00YKK0PTON2";
-            var url = 'https://api.foursquare.com/v2/venues/' + marker.venue + '?client_id='+ clientID +'&client_secret=' + clientSecret + '&v=20171204'
-			$.getJSON(url , function(data) {
-			  var res = data.response.venue;
-			  var name = res.name;
-			  var address = res.location.address;
-			  var city = res.location.city;
-			  var state = res.location.state;
-			  var zipcode = res.location.postalCode;
-
-			  foursquare = '<div><p><b>' + name + '</b></p>' +
-			  '<p>' + address + '</p>' +
-			  '<p>' + city + ', ' + state + '</p>' +
-			  '<p>' + zipcode + '</p></div>'
-
-			  // Use streetview service to get the closest streetview image within
-	          // 50 meters of the markers position
-	          streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-			})
-			  .fail(function() {
-			    alert( "Could not get venue information from Foursquare. Please refresh and try again." );
-			  });
-
-			// street view
-			var streetViewService = new google.maps.StreetViewService();
-	          var radius = 50;
-	          // In case the status is OK, which means the pano was found, compute the
-	          // position of the streetview image, then calculate the heading, then get a
-	          // panorama from that and set the options
-	          function getStreetView(data, status) {
-	            if (status == google.maps.StreetViewStatus.OK) {
-	              var nearStreetViewLocation = data.location.latLng;
-	              var heading = google.maps.geometry.spherical.computeHeading(
-	                nearStreetViewLocation, marker.position);
-	                infowindow.setContent(foursquare + '</div><div id="pano"></div>');
-	                var panoramaOptions = {
-	                  position: nearStreetViewLocation,
-	                  pov: {
-	                    heading: heading,
-	                    pitch: 30
-	                  }
-	                };
-	              var panorama = new google.maps.StreetViewPanorama(
-	                document.getElementById('pano'), panoramaOptions);
-	            } else {
-	              infowindow.setContent('<div>' + marker.title + '</div>' +
-	                '<div>No Street View Found</div>');
-	            }
-	          }
-	         
-	          // Open the infowindow on the correct marker.
-	          infowindow.open(map, marker);
-		}
-
-
-	// initialize map
-	this.markers = [];
-	this.initMap = function() {
-
-		// map constructor
-		map = new google.maps.Map(document.getElementById('map'), {
-		  zoom: 13,
-		  center: {lat: 47.608013, lng: -122.335167}
+// put marker info in infowindow
+function populateInfoWindow(marker, infowindow) {
+	if (infowindow.marker !== marker) {
+		infowindow.marker = marker;
+		infowindow.setContent('<div>' + marker.title + '</div>');
+		infowindow.open(map, marker);
+		// make sure marker property cleared if infowindow is closed
+		infowindow.addListener('closeclick', function () {
+			infowindow = null;
 		});
-
-		this.largeInfowindow = new google.maps.InfoWindow();
-
-		var locations = [
-			{title: 'Pike Place Market', location: {lat: 47.6101359, lng: -122.3420567}, venue: '427ea800f964a520b1211fe3'},
-			{title: 'Space Needle', location: {lat: 47.620423, lng: -122.349355}, venue: '416dc180f964a5209b1d1fe3'},
-			{title: 'Fremont Troll', location: {lat: 47.649682, lng: -122.347366}, venue: '49de874bf964a5206c601fe3'},
-			{title: 'Seattle Great Wheel', location: {lat: 47.606106, lng: -122.341530}, venue: '4fbfa89ee4b0fddeca5b5cef'},
-			{title: 'Olympic Sculpture Park', location: {lat: 47.616596, lng: -122.35531}, venue: '45b348a3f964a520a9411fe3'}
-		]
-
-		for(var i = 0; i < locations.length; i++){
-			// get position and title
-			var position = locations[i].location;
-			var title = locations[i].title;
-			var venue = locations[i].venue;
-			// create marker
-			var marker = new google.maps.Marker({
-				map: map,
-				position: position,
-				title: title,
-				venue: venue,
-				animation: google.maps.Animation.DROP,
-				id: i
-			});
-			// push marker to marker array
-			this.markers.push(marker);
-			// onclick event to open infowindow for marker
-			marker.addListener('click', self.openMarker);
-
-		}
 	}
 
-	// open marker details from left panel list
-	this.openMarker = function() {
-		self.populateInfoWindow(this, self.largeInfowindow);
-		this.setAnimation(google.maps.Animation.BOUNCE);
-			setTimeout((function() {
-        	this.setAnimation(null);
-    	}).bind(this), 800);
-	}
+	// foursquare api
+    clientID = "PVY15A1THVOBNAJETF2AZDRFGIY5YJKARWIAU2YWOIS2UQWH";
+    clientSecret = "XPETPWZCF0P45QLZOYUBOY5W55SPP1JYSJSEL00YKK0PTON2";
+    var url = 'https://api.foursquare.com/v2/venues/' + marker.venue + '?client_id='+ clientID +'&client_secret=' + clientSecret + '&v=20171204'
+	$.getJSON(url , function(data) {
+	  var res = data.response.venue;
+	  var name = res.name;
+	  var address = res.location.address;
+	  var city = res.location.city;
+	  var state = res.location.state;
+	  var zipcode = res.location.postalCode;
 
+	  foursquare = '<div><p><b>' + name + '</b></p>' +
+	  '<p>' + address + '</p>' +
+	  '<p>' + city + ', ' + state + '</p>' +
+	  '<p>' + zipcode + '</p></div>'
+
+	  // Use streetview service to get the closest streetview image within
+      // 50 meters of the markers position
+      streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+	})
+	  .fail(function() {
+	    alert( "Could not get venue information from Foursquare. Please refresh and try again." );
+	  });
+
+	// street view
+	var streetViewService = new google.maps.StreetViewService();
+      var radius = 50;
+      // In case the status is OK, which means the pano was found, compute the
+      // position of the streetview image, then calculate the heading, then get a
+      // panorama from that and set the options
+      function getStreetView(data, status) {
+        if (status == google.maps.StreetViewStatus.OK) {
+          var nearStreetViewLocation = data.location.latLng;
+          var heading = google.maps.geometry.spherical.computeHeading(
+            nearStreetViewLocation, marker.position);
+            infowindow.setContent(foursquare + '</div><div id="pano"></div>');
+            var panoramaOptions = {
+              position: nearStreetViewLocation,
+              pov: {
+                heading: heading,
+                pitch: 30
+              }
+            };
+          var panorama = new google.maps.StreetViewPanorama(
+            document.getElementById('pano'), panoramaOptions);
+        } else {
+          infowindow.setContent('<div>' + marker.title + '</div>' +
+            '<div>No Street View Found</div>');
+        }
+      }
+     
+      // Open the infowindow on the correct marker.
+      infowindow.open(map, marker);
+}
+
+
+// initialize map
+var markers = [];
+var largeInfowindow;
+function initMap() {
+
+	// map constructor
+	map = new google.maps.Map(document.getElementById('map'), {
+	  zoom: 13,
+	  center: {lat: 47.608013, lng: -122.335167}
+	});
+
+	// initialize infowindow
+	largeInfowindow = new google.maps.InfoWindow();
+
+	var locations = [
+		{title: 'Pike Place Market', location: {lat: 47.6101359, lng: -122.3420567}, venue: '427ea800f964a520b1211fe3'},
+		{title: 'Space Needle', location: {lat: 47.620423, lng: -122.349355}, venue: '416dc180f964a5209b1d1fe3'},
+		{title: 'Fremont Troll', location: {lat: 47.649682, lng: -122.347366}, venue: '49de874bf964a5206c601fe3'},
+		{title: 'Seattle Great Wheel', location: {lat: 47.606106, lng: -122.341530}, venue: '4fbfa89ee4b0fddeca5b5cef'},
+		{title: 'Olympic Sculpture Park', location: {lat: 47.616596, lng: -122.35531}, venue: '45b348a3f964a520a9411fe3'}
+	]
+
+	// create markers and add to markers array
+	for(var i = 0; i < locations.length; i++){
+		// get position and title
+		var position = locations[i].location;
+		var title = locations[i].title;
+		var venue = locations[i].venue;
+		// create marker
+		var marker = new google.maps.Marker({
+			map: map,
+			position: position,
+			title: title,
+			venue: venue,
+			animation: google.maps.Animation.DROP,
+			id: i
+		});
+		// push marker to marker array
+		this.markers.push(marker);
+		// onclick event to open infowindow for marker
+		marker.addListener('click', openMarker);
+
+	}
+}
+
+// open marker details and animate
+function openMarker() {
+	populateInfoWindow(this, largeInfowindow);
+	this.setAnimation(google.maps.Animation.BOUNCE);
+		setTimeout((function() {
+    	this.setAnimation(null);
+	}).bind(this), 800);
+}
+
+// view model
+function AppViewModel() {
 	// initialize map
-	this.initMap();
-
+	initMap();
 
 	// filter list of places and hide markers
 	this.searched = ko.observable("");
 
 	this.filter = ko.computed(function() {
-		this.largeInfowindow.close();
+		largeInfowindow.close();
 		var filtered = [];
-	    for (i = 0; i < this.markers.length; i++) {
-
-	        if (this.markers[i].title.toUpperCase().indexOf(this.searched().toUpperCase()) > -1) {
-	            this.markers[i].setVisible(true);
-	            filtered.push(this.markers[i]);
+	    for (i = 0; i < markers.length; i++) {
+	    	// check if searched term matches any of the location titles & change visiblity accordingly
+	        if (markers[i].title.toUpperCase().indexOf(this.searched().toUpperCase()) > -1) {
+	            markers[i].setVisible(true);
+	            filtered.push(markers[i]);
 	        } else {
-	            this.markers[i].setVisible(false);
+	            markers[i].setVisible(false);
 	        }
 	    }
 		return filtered;    
 	}, this);
 
-
 }
 
+// error handling for Google API
 function handleError() {
 	alert('Could not load Google API. Please refresh and try again.');
 }
 
+// call back function to start application
 function startApp() {
     ko.applyBindings(new AppViewModel());
 }
